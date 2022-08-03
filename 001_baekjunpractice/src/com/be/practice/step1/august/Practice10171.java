c72a478800e80848a069eb192b1e23c88f74182a
thusis <102267039+thusis@users.noreply.github.com>
2022-08-03 11:03:36 +0900

    고양이 완료

--- 001_baekjunpractice/src/com/be/practice/step1/august/Practice10171.java
+++ 001_baekjunpractice/src/com/be/practice/step1/august/Practice10171.java
@@ -1,9 +1,7 @@
 package com.be.practice.step1.august;
 
 public class Practice10171 {
-	public static void main(String[] args){
-		System.out.println("\\    /\\ \n)  ( ') \n(  /  ) \n\\(__)| ");
-	}
-}
-
-// 고양이랑 출력 또옥같이
+	    public static void main(String[] args){
+	        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|\n");
+	        }
+	    }
