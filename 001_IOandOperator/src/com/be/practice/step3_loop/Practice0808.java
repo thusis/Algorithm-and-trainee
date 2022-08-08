2736fcbfb680410c2bca223d78039ad14868968a
thusis <102267039+thusis@users.noreply.github.com>
2022-08-09 00:00:14 +0900

    202-

---
+++ 001_IOandOperator/src/com/be/practice/step3_loop/Practice0808.java
@@ -0,0 +1,16 @@
+package com.be.practice.step3_loop;
+
+public class Practice0808 {
+	public static void method() {}
+	package com.kh.example.chap04_user.model.run;
+
+	import com.kh.example.chap04_user.model.controller.UserExceptionController;
+
+	public class Run {
+		public static void main(String[] args) {
+			new UserExceptionController().inputAge();
+		}
+	}
+
+
+}
