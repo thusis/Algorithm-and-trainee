42e7917c3a39d90b6d1564087eb3d867239e1465
thusis <102267039+thusis@users.noreply.github.com>
2022-08-10 23:30:05 +0900

    2022-08-10

---
+++ 001_IOandOperator/src/com/be/practice/step3_loop/Practice25304.java
@@ -0,0 +1,22 @@
+package com.be.practice.step3_loop;
+
+import java.util.Scanner;
+
+public class Practice25304 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sum += a * b;
+		}
+		if (X == sum) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
