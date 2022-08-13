3219c7e25ea82a84a18f25d708eba6ef8c1346be
thusis <102267039+thusis@users.noreply.github.com>
2022-08-13 20:13:10 +0900

    2022-08-13

---
+++ 001_August/src/com/be/practice/step3_loop/Practice10871.java
@@ -0,0 +1,21 @@
+package com.be.practice.step3_loop;
+
+import java.util.Scanner;
+
+public class Practice10871 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i=0; i<N ; i++) {
+			int A = sc.nextInt();
+			arr[i] = A;
+		}
+		for(int i=0; i<N ; i++) {
+			if(arr[i]<X) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
--- 001_August/src/com/be/practice/step3_loop/Practicelala.java
+++
@@ -1,7 +0,0 @@
-package com.be.practice.step3_loop;
-
-public class Practicelala {
-	public static void main(String[] args) {
-		mainMenu(){}
-	}
-}
