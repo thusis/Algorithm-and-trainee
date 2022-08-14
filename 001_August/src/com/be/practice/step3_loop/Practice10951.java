91a51d5166b86bd78b67ccaec24e492ad707fd85
thusis <102267039+thusis@users.noreply.github.com>
2022-08-14 23:57:28 +0900

    2022-08-14

---
+++ 001_August/src/com/be/practice/step3_loop/Practice10951.java
@@ -0,0 +1,16 @@
+package com.be.practice.step3_loop;
+
+import java.util.Scanner;
+
+public class Practice10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+						
+			System.out.println(a+b);
+		}
+	}
+}
---
+++ 001_August/src/com/be/practice/step3_loop/Practice1110.java
@@ -0,0 +1,5 @@
+package com.be.practice.step3_loop;
+
+public class Practice1110 {
+
+}
