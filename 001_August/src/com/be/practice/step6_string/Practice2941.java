b5ce71d6af34553558a96d4b871bf46967867e69
thusis <102267039+thusis@users.noreply.github.com>
2022-08-28 23:46:11 +0900

    2022-08-28

---
+++ 001_August/src/com/be/practice/step6_string/Practice2941.java
@@ -0,0 +1,41 @@
+package com.be.practice.step6_string;
+
+import java.util.Scanner;
+
+public class Practice2941 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int count = 0;
+		
+		for(int i =0; i<s.length(); i++) {
+			count ++;
+			switch(s.charAt(i)) {
+			case 'c':
+				if((s.charAt(i+1) == '=') || (s.charAt(i+1) == '-') ) {
+					i++;
+					break;
+				}				
+			case 'd':
+				if((s.charAt(i+1) == 'z') && (s.charAt(i+2) == '=')) {
+					i+=2;
+					break;
+				}else if(s.charAt(i+1) == '-') {
+					i++;
+					break;
+				}
+			case 'l': case 'n':
+				if(s.charAt(i+1)=='j') {
+					i++;
+					break;
+				}
+			case 's': case 'z':
+				if(s.charAt(i+1)=='=') {
+					i++;
+					break;
+				}				
+			}
+		}
+		System.out.println(count);
+	}
+}
