aa0d355082d56019c51556a06e29045dd580d0c9
thusis <102267039+thusis@users.noreply.github.com>
2022-08-29 15:00:52 +0900

    2022-08-29

--- 001_August/src/com/be/practice/step6_string/Practice1316.java
+++ 001_August/src/com/be/practice/step6_string/Practice1316.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Practice1316 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Character> l = new ArrayList<Character>();
@@ -14,15 +13,20 @@ public class Practice1316 {
 		
 		for(int i=0; i<N; i++) {
 			String s = sc.next();
-			
+			if(!l.isEmpty()) {
+				l.clear();
+			}
 			for(int j=0; j<s.length(); j++) {
-				l.add(s.charAt(i));
-				if (l.contains(s.charAt(i))) {
-//					연속된 값이면 ㅇㅋ
-//					비연속이면 count -= 1;
+				if(!l.contains(s.charAt(j))){
+					l.add(s.charAt(j));
+				} else {
+					if(s.charAt(j) != s.charAt(j-1)) {
+						count-=1;
+						break;
+					}
 				}
 			}
 		}
+		System.out.println(count);
 	}
-
 }
