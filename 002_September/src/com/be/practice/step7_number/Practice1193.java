9797179e814e38ee2f205a7a3b79bbe356618edf
thusis <102267039+thusis@users.noreply.github.com>
2022-09-06 23:16:22 +0900

    2022-09-06

--- 002_September/src/com/be/practice/step7_number/Practice1193.java
+++ 002_September/src/com/be/practice/step7_number/Practice1193.java
@@ -11,51 +11,28 @@ public class Practice1193 {
 		
 		ArrayList<Integer> bunja = new ArrayList<Integer>();
 		ArrayList<Integer> bunmo = new ArrayList<Integer>();
-//		ArrayList<String> result = new ArrayList<String>();
 		
-		for(int i=1; i<X*4;i+=2) { //2씩 커지는 최대값, i의 범위를 어떻게 설정해야 할지 고민된다.
-			
+		for(int i=1; bunja.size()>X ;i+=2) { 
 			int j=0;
-			while (j!=i) { j++; bunja.add(j); } //123..12345..1234567..
-			while (j!=1) { j--;bunja.add(j); } //21..4321..654321..
-			while (j!=i+1) { j++; bunmo.add(j); } //12..1234..123456..
-			while (j!=1) { j--;bunmo.add(j); }//1..321..54321..
+			while (j!=i) { j++; bunja.add(j);} 
+			while (j!=1) { j--;bunja.add(j);}
 			
-			if(Math.min(bunja.size(), bunmo.size())>X) {
-				System.out.println(bunja.get(X)+"/"+bunmo.get(X));
+			if(bunja.size()>X) {break;}
+			
+		}
+		
+		for(int i=1; bunmo.size()>X; i+=2) {
+			int j=0;
+			while (j!=i+1) { j++; bunmo.add(j);}
+			while (j!=1) { j--;bunmo.add(j);}
+			if(bunmo.size()>X) {
+				System.out.println(bunja.get(X-1) + "/" + bunmo.get(X-1));
+
 				break;
 			}
 		}
-		
+		sc.close();
+		System.out.println(bunmo);
+		System.out.println(bunja);
 	}
-	
-
-	
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		int X = sc.nextInt();
-//		
-//		ArrayList<Integer> bunja = new ArrayList<Integer>();
-//		ArrayList<Integer> bunmo = new ArrayList<Integer>();
-//		
-//		for(int i=1; i<X*10 ;i+=2) { //2씩 커지는 최대값, i의 범위를 어떻게 설정해야 할지 고민된다.
-//			
-//			int j=0;
-//			while (j!=i) { j++; bunja.add(j); } //123..12345..1234567..
-//			while (j!=1) { j--;bunja.add(j); } //21..4321..654321..
-//			while (j!=i+1) { j++; bunmo.add(j); } //12..1234..123456..
-//			while (j!=1) { j--;bunmo.add(j); }//1..321..54321..
-//		}
-//		
-//		System.out.println("["+X + ":" + bunja.get(X)+"/"+bunmo.get(X)+"] ");
-//		
-//		
-//		
-////		for(int i=2; i<10 ;i+=2) {
-////			int j=0;
-////			while (j!= i) { j++; bunmo.add(j); }
-////			while (j!=1) { j--;bunmo.add(j); }
-////		}
-//	}
-	
 }
