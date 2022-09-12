34ffea12b8d48f70d358b6a1ac15ff2dccd9fba9
thusis <102267039+thusis@users.noreply.github.com>
2022-09-12 14:55:26 +0900

    2022-09-12
    두번째 커밋

--- 002_September/src/com/be/practice/step8_number/Practice2581.java
+++ 002_September/src/com/be/practice/step8_number/Practice2581.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Practice2581 {
 	public static void main(String[] args) {
-		//소수
+
+		// 소수를 작성할거고 언니한테 보여줘야지
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+		int X1 = sc.nextInt();
+		
+		System.out.println("두번째 커밋");
 	}
 }
