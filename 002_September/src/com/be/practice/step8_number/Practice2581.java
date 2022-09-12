99a3437d4e1792728e7d85570c23c25c8f11179e
thusis <102267039+thusis@users.noreply.github.com>
2022-09-12 14:53:49 +0900

    2022-09-12

--- 002_September/src/com/be/practice/step8_number/Practice2581.java
+++ 002_September/src/com/be/practice/step8_number/Practice2581.java
@@ -1,7 +1,11 @@
 package com.be.practice.step8_number;
 
+import java.util.Scanner;
+
 public class Practice2581 {
 	public static void main(String[] args) {
 		//소수
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
 	}
 }
