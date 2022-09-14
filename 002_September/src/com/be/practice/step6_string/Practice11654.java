1c7c47c1fd3cc7ede912b72319961a39013f1147
thusis <102267039+thusis@users.noreply.github.com>
2022-09-14 15:02:31 +0900

    2022-09-14

---
+++ 002_September/src/com/be/practice/step6_string/Practice11654.java
@@ -0,0 +1,13 @@
+package com.be.practice.step6_string;
+
+import java.util.Scanner;
+
+public class Practice11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		int n = (int)(ch);
+		System.out.println(n);
+	}
+
+}
