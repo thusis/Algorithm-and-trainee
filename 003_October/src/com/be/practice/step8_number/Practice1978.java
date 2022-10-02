25f9e12540bace264342fc76aead416ac9ce2b78
thusis <102267039+thusis@users.noreply.github.com>
2022-10-02 20:00:40 +0900

    2022-10-01

--- 003_October/src/com/be/practice/step8_number/Practice1978.java
+++ 003_October/src/com/be/practice/step8_number/Practice1978.java
@@ -11,12 +11,8 @@ public class Practice1978 {
 		int count = 0;
 		
 		String[] arr=(sc.nextLine()).split(" ");
-		System.out.println(arr);
 		for(int m=0; m<n; m++) {
-			System.out.println("arr["+m+"]은"+arr[m]+"입니다");
 			x = Integer.parseInt(arr[m]);
-			System.out.println("x는 "+ x+"입니다");
-			
 			boolean result = false;		
 			if(x==2) {
 				result=true;
@@ -30,13 +26,10 @@ public class Practice1978 {
 							break;// 소수이면 true 반환
 						}}}}
 			if(result) {
-				System.out.println(x+"는 소수입니다.");		
 				count++;
-			}else {
-				System.out.println(x+"는 소수가 아닙니다.");
 			}
 		}
-		System.out.println("소수는 총 "+count+"개입니다.");
+		System.out.println(count);
 		
 
 	}
