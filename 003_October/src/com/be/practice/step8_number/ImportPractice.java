224631f0a7bffd59e30ac401b029fdd696a16547
thusis <102267039+thusis@users.noreply.github.com>
2022-10-07 09:57:37 +0900

    2022-10-07

---
+++ 003_October/src/com/be/practice/step8_number/ImportPractice.java
@@ -0,0 +1,9 @@
+package com.be.practice.step8_number;
+
+import java.util.HashMap;
+
+public class ImportPractice {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+	}
+}
