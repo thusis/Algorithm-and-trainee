a9f4eb8e188a37827df06226ad0ccc79ebbad009
thusis <102267039+thusis@users.noreply.github.com>
2022-10-15 23:53:07 +0900

    2022-10-15

---
+++ 003_October/src/com/be/practice/P2750.java
@@ -0,0 +1,23 @@
+package com.be.practice;
+
+import java.util.Scanner;
+
+public class P2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+			System.out.println(arr[i]);
+		}
+		for(int i=0; i<n-1; i++) {
+			for(int j=0; j<j-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					
+				}
+			}
+		}
+	}
+
+}
