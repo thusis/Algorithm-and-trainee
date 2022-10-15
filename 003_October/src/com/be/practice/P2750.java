cc48c19ecac8320f10676c69289146851115e852
thusis <102267039+thusis@users.noreply.github.com>
2022-10-15 23:57:12 +0900

    2022-10-15

--- 003_October/src/com/be/practice/P2750.java
+++ 003_October/src/com/be/practice/P2750.java
@@ -9,14 +9,19 @@ public class P2750 {
 		int[] arr = new int[n];
 		for(int i=0; i<n; i++) {
 			arr[i] = sc.nextInt();
-			System.out.println(arr[i]);
 		}
+		int temp=0;
 		for(int i=0; i<n-1; i++) {
-			for(int j=0; j<j-1; j++) {
+			for(int j=0; j<n-1; j++) {
 				if(arr[j]>arr[j+1]) {
-					
+					temp = arr[j];
+					arr[j]= arr[j+1];
+					arr[j+1]=temp;
 				}
 			}
+		}
+		for(int i=0; i<n; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 
